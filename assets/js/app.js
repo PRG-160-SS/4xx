ece0138b5b4ae1217916a4f46807ded48c68bb67
[savannahswartzel] <[ss55484@ravens.racc.edu]>
2018-03-11 17:18:12 -0400

    added prgress bar and displayPB function. Also  added cdn link for bootstrap in the index.html file

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,21 +27,47 @@ function initializeApplication() {
     elHeaderTitle.innerHTML = appData.title;
     elHeader.appendChild(elHeaderTitle);
     elHeaderTitle.className = 'animated bounceInDown';
-    
+
     var elHeaderTagLine = document.createElement('h2');
     elHeaderTagLine.textContent = appData.tagLine;
     elHeaderTagLine.style.fontSize = '.4em';
     elHeader.appendChild(elHeaderTagLine);
     elHeaderTagLine.className = 'animated bounceInLeft';
-    
+
     var elMain = document.createElement('main');
     elMain.innerHTML = '<p>Hello World!</p>';
     elWrapper.appendChild(elMain);
     elMain.className = 'animated zoomIn';
-    
+
     var elFooter = document.createElement('footer');
     elFooter.innerHTML = '<h4>get excited about learning JavaScript...</h4>';
     elWrapper.appendChild(elFooter);
     elFooter.className = 'animated bounceInRight';
 
+    elMain.innerHTML += '<div style="width: 40%; margin: auto; padding:10px; margin-bottom: 20px;"><div class="progress" style="height: 20px;"><div id="loaderProgressBar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div></div></div>';
+    
+    displayPB();
 }
+
+
+var timerCount = 0;
+function displayPB() {
+    if (timerCount <= 100) {
+        var x = (timerCount < 25) ? '' : (timerCount < 45) ? timerCount + '%' : (timerCount < 65) ? 'Loading ' + timerCount + '%' : 'Loading Application ' + timerCount + '%';
+        document.getElementById("loaderProgressBar").innerHTML = x;
+        document.getElementById('loaderProgressBar').setAttribute('aria-valuenow', timerCount);
+        document.getElementById('loaderProgressBar').style.width = timerCount + '%    ';
+        timerCount++;
+        setTimeout(displayPB, 50);
+    } else {
+        timerCount = 0;
+        return false;
+    }
+}
+
+
+
+
+
+
+
